4784a687efe9aa1d26142f6b0b271e234505fcb5
Andrii Beznosov <andrey301288@gmail.com>
2022-09-03 22:40:15 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('Hello!');
+import { Notify } from 'notiflix';
+
+Notify.success('Sol lucet omnibus');
+//<div class="photo-card">
+//   <img src="" alt="" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//     </p>
+//   </div>
+// </div>
+//<button type="button" class="load-more">Load more</button>
